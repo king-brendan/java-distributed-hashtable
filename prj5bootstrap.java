748236c787a348c21a6c14fc63348094c6ea35b5
king-brendan <31732221+king-brendan@users.noreply.github.com>
2023-04-21 13:29:09 -0400

    wahoo

---
+++ prj5bootstrap.java
@@ -0,0 +1,5 @@
+public class prj5bootstrap {
+    public static void main(String[] args) {
+        System.out.println("bootie boot");
+    }
+}
---
+++ prj5peer.java
@@ -0,0 +1,5 @@
+public class prj5peer {
+    public static void main(String[] args) {
+        System.out.println("peer into me");
+    }
+}
